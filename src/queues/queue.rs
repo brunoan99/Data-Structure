64407f12de3f3457e9e4918a4a738146b03f3cdf
Bruno Andrade <brunoan99@gmail.com>
2023-10-12 06:12:54 -0300

    feat(queues): add drop, rev, concat and split methods

--- src/queues/queue.rs
+++ src/queues/queue.rs
@@ -1,4 +1,4 @@
-pub use crate::Stack;
+use crate::Stack;
 
 #[derive(Clone, PartialEq, Debug)]
 pub struct Queue<T> {
@@ -36,13 +36,10 @@ where
       Self {
         head,
         tail: Stack::Empty,
-      } => {
-        let rev_head = Stack::rev(&head);
-        Self::dequeue(&Self {
-          head: Stack::Empty,
-          tail: rev_head,
-        })
-      }
+      } => Self::dequeue(&Self {
+        head: Stack::Empty,
+        tail: Stack::rev(&head),
+      }),
       Self { head, tail } => {
         let (poped, new_tail) = Stack::pop(&tail).unwrap();
         Some((
@@ -56,7 +53,27 @@ where
     }
   }
 
-  pub fn peek(queue: &Self) -> Option<T> {
+  pub fn drop(queue: &Self) -> Option<Self> {
+    match &queue {
+      Self {
+        head: Stack::Empty,
+        tail: Stack::Empty,
+      } => None,
+      Self {
+        head,
+        tail: Stack::Empty,
+      } => Self::drop(&Self {
+        head: Stack::Empty,
+        tail: Stack::rev(&head),
+      }),
+      Self { head, tail } => Some(Self {
+        head: head.to_owned(),
+        tail: Stack::pop(&tail).unwrap().1,
+      }),
+    }
+  }
+
+  pub fn get(queue: &Self) -> Option<T> {
     match &queue {
       Self {
         head: Stack::Empty,
@@ -76,6 +93,35 @@ where
 
   pub fn len(queue: &Self) -> i32 {
     Stack::len(&queue.head) + Stack::len(&queue.tail)
+  }
+
+  pub fn rev(queue: &Self) -> Self {
+    match &queue {
+      Queue {
+        head: Stack::Node(..),
+        tail: Stack::Node(..),
+      } => Queue {
+        head: queue.tail.to_owned(),
+        tail: queue.head.to_owned(),
+      },
+      Queue { head, tail } => Queue {
+        head: Stack::rev(&head),
+        tail: Stack::rev(&tail),
+      },
+    }
+  }
+
+  pub fn concat(q1: &Self, q2: &Self) -> Self {
+    Self {
+      head: Stack::concat(&Stack::rev(&q2.tail), &q2.head),
+      tail: Stack::concat(&Stack::rev(&q1.head), &q1.tail),
+    }
+  }
+
+  pub fn split(queue: &Self, f: fn(&T) -> bool) -> (Self, Self) {
+    let (h1, h2) = Stack::split(&queue.head, f);
+    let (t1, t2) = Stack::split(&queue.tail, f);
+    (Self { head: h1, tail: t1 }, Self { head: h2, tail: t2 })
   }
 }
 
--- src/queues/queue_test.rs
+++ src/queues/queue_test.rs
@@ -2,6 +2,10 @@ use super::*;
 
 type QueueT = Queue<i32>;
 
+fn node<T>(head: T, tail: Stack<T>) -> Stack<T> {
+  Stack::Node(head, Box::new(tail))
+}
+
 fn queue_empty_on_both() -> QueueT {
   QueueT {
     head: Stack::<i32>::Empty,
@@ -11,16 +15,7 @@ fn queue_empty_on_both() -> QueueT {
 
 fn queue_filled_on_head() -> Queue<i32> {
   Queue {
-    head: Stack::Node(
-      3,
-      Box::new(Stack::Node(
-        2,
-        Box::new(Stack::Node(
-          1,
-          Box::new(Stack::Node(0, Box::new(Stack::Empty))),
-        )),
-      )),
-    ),
+    head: node(3, node(2, node(1, node(0, Stack::Empty)))),
     tail: Stack::Empty,
   }
 }
@@ -28,238 +23,363 @@ fn queue_filled_on_head() -> Queue<i32> {
 fn queue_filled_on_tail() -> Queue<i32> {
   Queue {
     head: Stack::Empty,
-    tail: Stack::Node(
-      0,
-      Box::new(Stack::Node(
-        1,
-        Box::new(Stack::Node(
-          2,
-          Box::new(Stack::Node(3, Box::new(Stack::Empty))),
-        )),
-      )),
-    ),
+    tail: node(0, node(1, node(2, node(3, Stack::Empty)))),
   }
 }
 
 fn queue_filled_on_both() -> Queue<i32> {
   Queue {
-    head: Stack::Node(
-      7,
-      Box::new(Stack::Node(
-        6,
-        Box::new(Stack::Node(
-          5,
-          Box::new(Stack::Node(4, Box::new(Stack::Empty))),
-        )),
-      )),
-    ),
-    tail: Stack::Node(
-      0,
-      Box::new(Stack::Node(
-        1,
-        Box::new(Stack::Node(
-          2,
-          Box::new(Stack::Node(3, Box::new(Stack::Empty))),
-        )),
-      )),
-    ),
+    head: node(7, node(6, node(5, node(4, Stack::Empty)))),
+    tail: node(0, node(1, node(2, node(3, Stack::Empty)))),
   }
 }
 
 #[test]
-fn test_is_empty_to_both_empty_on_both() {
+fn is_empty_to_both_empty_on_both() {
   let queue = queue_empty_on_both();
   let op = Queue::empty(&queue);
   assert_eq!(op, true)
 }
 
 #[test]
-fn test_is_empty_to_filled_on_head() {
+fn is_empty_to_filled_on_head() {
   let queue = queue_filled_on_head();
   let op = Queue::empty(&queue);
   assert_eq!(op, false)
 }
 
 #[test]
-fn test_is_empty_filled_on_tail() {
+fn is_empty_filled_on_tail() {
   let queue = queue_filled_on_tail();
   let op = Queue::empty(&queue);
   assert_eq!(op, false)
 }
 
 #[test]
-fn test_is_empty_to_filled_on_both() {
+fn is_empty_to_filled_on_both() {
   let queue = queue_filled_on_both();
   let op = Queue::empty(&queue);
   assert_eq!(op, false)
 }
 
 #[test]
-fn test_enqueue_to_empty_on_both() {
+fn enqueue_to_empty_on_both() {
   let queue = queue_empty_on_both();
   let op = Queue::enqueue(&queue, 0);
   let expected = QueueT {
-    head: Stack::Node(0, Box::new(Stack::Empty)),
+    head: node(0, Stack::Empty),
     tail: Stack::Empty,
   };
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_enqueue_to_filled_on_head() {
+fn enqueue_to_filled_on_head() {
   let queue = queue_filled_on_head();
-  let op = Queue::enqueue(&queue, 9);
+  let op = Queue::enqueue(&queue, 4);
   let expected = QueueT {
-    head: Stack::Node(9, Box::new(queue.head)),
+    head: node(4, queue.head),
     tail: Stack::Empty,
   };
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_enqueue_to_filled_on_tail() {
+fn enqueue_to_filled_on_tail() {
   let queue = queue_filled_on_tail();
-  let op = Queue::enqueue(&queue, 9);
+  let op = Queue::enqueue(&queue, 4);
   let expected = QueueT {
-    head: Stack::Node(9, Box::new(Stack::Empty)),
+    head: node(4, Stack::Empty),
     tail: queue.tail,
   };
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_enqueue_to_filled_on_both() {
+fn enqueue_to_filled_on_both() {
   let queue = queue_filled_on_both();
-  let op = Queue::enqueue(&queue, 9);
+  let op = Queue::enqueue(&queue, 8);
   let expected = QueueT {
-    head: Stack::Node(9, Box::new(queue.head)),
+    head: node(8, queue.head),
     tail: queue.tail,
   };
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_dequeue_to_empty_on_both() {
+fn dequeue_to_empty_on_both() {
   let queue = queue_empty_on_both();
   let op = Queue::dequeue(&queue);
   assert_eq!(op, None)
 }
 
 #[test]
-fn test_dequeue_to_filled_on_head() {
+fn dequeue_to_filled_on_head() {
   let queue = queue_filled_on_head();
   let op = Queue::dequeue(&queue);
-  let expected_queue = QueueT {
-    head: Stack::Empty,
-    tail: Stack::Node(
-      1,
-      Box::new(Stack::Node(
-        2,
-        Box::new(Stack::Node(3, Box::new(Stack::Empty))),
-      )),
-    ),
-  };
-  assert_eq!(op, Some((0, expected_queue)))
+  let expected = Some((
+    0,
+    Queue {
+      head: Stack::Empty,
+      tail: node(1, node(2, node(3, Stack::Empty))),
+    },
+  ));
+  assert_eq!(op, expected)
 }
 
 #[test]
-fn test_dequeue_to_filled_on_tail() {
+fn dequeue_to_filled_on_tail() {
   let queue = queue_filled_on_tail();
   let op = Queue::dequeue(&queue);
-  let expected_queue = QueueT {
-    head: Stack::Empty,
-    tail: Stack::Node(
-      1,
-      Box::new(Stack::Node(
-        2,
-        Box::new(Stack::Node(3, Box::new(Stack::Empty))),
-      )),
-    ),
-  };
-  assert_eq!(op, Some((0, expected_queue)))
+  let expected = Some((
+    0,
+    Queue {
+      head: Stack::Empty,
+      tail: node(1, node(2, node(3, Stack::Empty))),
+    },
+  ));
+  assert_eq!(op, expected)
 }
 
 #[test]
-fn test_dequeue_to_filled_on_both() {
+fn dequeue_to_filled_on_both() {
   let queue = queue_filled_on_both();
   let op = Queue::dequeue(&queue);
-  let expected_queue = Queue {
-    head: Stack::Node(
-      7,
-      Box::new(Stack::Node(
-        6,
-        Box::new(Stack::Node(
-          5,
-          Box::new(Stack::Node(4, Box::new(Stack::Empty))),
-        )),
-      )),
-    ),
-    tail: Stack::Node(
-      1,
-      Box::new(Stack::Node(
-        2,
-        Box::new(Stack::Node(3, Box::new(Stack::Empty))),
-      )),
-    ),
-  };
-  assert_eq!(op, Some((0, expected_queue)))
+  let expected = Some((
+    0,
+    Queue {
+      head: node(7, node(6, node(5, node(4, Stack::Empty)))),
+      tail: node(1, node(2, node(3, Stack::Empty))),
+    },
+  ));
+  assert_eq!(op, expected)
 }
 
 #[test]
-fn test_peek_to_empty_on_both() {
+fn get_to_empty_on_both() {
   let queue = queue_empty_on_both();
-  let op = Queue::peek(&queue);
+  let op = Queue::get(&queue);
   assert_eq!(op, None)
 }
 
 #[test]
-fn test_peek_to_filled_on_head() {
+fn get_to_filled_on_head() {
   let queue = queue_filled_on_head();
-  let op = Queue::peek(&queue);
+  let op = Queue::get(&queue);
   let expected = Some(0);
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_peek_to_filled_on_tail() {
+fn get_to_filled_on_tail() {
   let queue = queue_filled_on_tail();
-  let op = Queue::peek(&queue);
+  let op = Queue::get(&queue);
   let expected = Some(0);
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_peek_to_filled_on_both() {
+fn get_to_filled_on_both() {
   let queue = queue_filled_on_both();
-  let op = Queue::peek(&queue);
+  let op = Queue::get(&queue);
   let expected = Some(0);
   assert_eq!(op, expected)
 }
 
 #[test]
-fn test_len_to_empty_on_both() {
+fn len_to_empty_on_both() {
   let queue = queue_empty_on_both();
   let op = Queue::len(&queue);
   assert_eq!(op, 0)
 }
 
 #[test]
-fn test_len_to_filled_on_head() {
+fn len_to_filled_on_head() {
   let queue = queue_filled_on_head();
   let op = Queue::len(&queue);
   assert_eq!(op, 4)
 }
 
 #[test]
-fn test_len_to_filled_on_tail() {
+fn len_to_filled_on_tail() {
   let queue = queue_filled_on_tail();
   let op = Queue::len(&queue);
   assert_eq!(op, 4)
 }
 
 #[test]
-fn test_len_to_filled_on_both() {
+fn len_to_filled_on_both() {
   let queue = queue_filled_on_both();
   let op = Queue::len(&queue);
   assert_eq!(op, 8)
+}
+
+#[test]
+fn rev_to_empty_on_both() {
+  let queue = queue_empty_on_both();
+  let op = Queue::rev(&queue);
+  assert_eq!(
+    op,
+    Queue {
+      head: Stack::Empty,
+      tail: Stack::Empty,
+    }
+  )
+}
+
+#[test]
+fn rev_to_filled_on_head() {
+  let queue = queue_filled_on_head();
+  let op = Queue::rev(&queue);
+  let expected = Queue {
+    head: node(0, node(1, node(2, node(3, Stack::Empty)))),
+    tail: Stack::Empty,
+  };
+  assert_eq!(op, expected)
+}
+
+#[test]
+fn rev_to_filled_on_tail() {
+  let queue = queue_filled_on_tail();
+  let op = Queue::rev(&queue);
+  let expected = Queue {
+    head: Stack::Empty,
+    tail: node(3, node(2, node(1, node(0, Stack::Empty)))),
+  };
+  assert_eq!(op, expected)
+}
+
+#[test]
+fn rev_to_filled_on_both() {
+  let queue = queue_filled_on_both();
+  let op = Queue::rev(&queue);
+  let expected = Queue {
+    head: node(0, node(1, node(2, node(3, Stack::Empty)))),
+    tail: node(7, node(6, node(5, node(4, Stack::Empty)))),
+  };
+  assert_eq!(op, expected)
+}
+
+#[test]
+fn concat_to_both_filled_on_both() {
+  let q1 = Queue {
+    head: node(4, node(3, Stack::Empty)),
+    tail: node(1, node(2, Stack::Empty)),
+  };
+  let q2 = Queue {
+    head: node(8, node(7, Stack::Empty)),
+    tail: node(5, node(6, Stack::Empty)),
+  };
+  let op = Queue::concat(&q1, &q2);
+  let expect = Queue {
+    head: node(8, node(7, node(6, node(5, Stack::Empty)))),
+    tail: node(1, node(2, node(3, node(4, Stack::Empty)))),
+  };
+  assert_eq!(op, expect)
+}
+
+#[test]
+fn split_to_empty_on_both() {
+  let queue = queue_empty_on_both();
+  let op = Queue::split(&queue, |item| item >= &1);
+  assert_eq!(
+    op,
+    (
+      Queue {
+        head: Stack::Empty,
+        tail: Stack::Empty,
+      },
+      Queue {
+        head: Stack::Empty,
+        tail: Stack::Empty,
+      }
+    )
+  )
+}
+
+#[test]
+fn split_to_filled_on_head() {
+  let queue = queue_filled_on_head();
+  let op = Queue::split(&queue, |item| item > &1);
+  assert_eq!(
+    op,
+    (
+      Queue {
+        head: node(1, node(0, Stack::Empty)),
+        tail: Stack::Empty,
+      },
+      Queue {
+        head: node(3, node(2, Stack::Empty)),
+        tail: Stack::Empty,
+      }
+    )
+  )
+}
+
+#[test]
+fn split_to_filled_on_tail() {
+  let queue = queue_filled_on_tail();
+  let op = Queue::split(&queue, |item| item > &1);
+  let expected = (
+    Queue {
+      head: Stack::Empty,
+      tail: node(0, node(1, Stack::Empty)),
+    },
+    Queue {
+      head: Stack::Empty,
+      tail: node(2, node(3, Stack::Empty)),
+    },
+  );
+  assert_eq!(op, expected)
+}
+
+#[test]
+fn split_to_filled_on_both() {
+  let queue = queue_filled_on_both();
+  let op = Queue::split(&queue, |item| item % 2 == 1);
+  let expected = (
+    Queue {
+      head: node(6, node(4, Stack::Empty)),
+      tail: node(0, node(2, Stack::Empty)),
+    },
+    Queue {
+      head: node(7, node(5, Stack::Empty)),
+      tail: node(1, node(3, Stack::Empty)),
+    },
+  );
+  assert_eq!(op, expected)
+}
+
+#[test]
+fn split_to_filled_on_both_with_only_false_return() {
+  let queue = queue_filled_on_both();
+  let op = Queue::split(&queue, |_| false);
+  let expected = (
+    Queue {
+      head: node(7, node(6, node(5, node(4, Stack::Empty)))),
+      tail: node(0, node(1, node(2, node(3, Stack::Empty)))),
+    },
+    Queue {
+      head: Stack::Empty,
+      tail: Stack::Empty,
+    },
+  );
+  assert_eq!(op, expected)
+}
+
+#[test]
+fn split_to_filled_on_both_with_only_true_return() {
+  let queue = queue_filled_on_both();
+  let op = Queue::split(&queue, |_| true);
+  let expected = (
+    Queue {
+      head: Stack::Empty,
+      tail: Stack::Empty,
+    },
+    Queue {
+      head: node(7, node(6, node(5, node(4, Stack::Empty)))),
+      tail: node(0, node(1, node(2, node(3, Stack::Empty)))),
+    },
+  );
+  assert_eq!(op, expected)
 }
