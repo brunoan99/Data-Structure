0ba1c1eff35aa4367591de5d7f7a4716b37ecde5
Bruno Andrade <brunoan99@gmail.com>
2023-11-08 01:38:50 -0300

    refactor(lists): remove unecessary traits and rename Node to ListNode

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,2 @@
-#[path = "./stacks/stack.rs"]
-pub mod stacks;
-pub use stacks::*;
-
-pub mod queues;
-pub use queues::*;
-
-pub mod lists;
-pub use lists::*;
-
-pub mod trees;
-pub use trees::*;
+pub mod persistent;
+pub use persistent::*;
